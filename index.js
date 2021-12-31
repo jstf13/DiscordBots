441ea1084d71bd8bb3860f6f094c44acca7ef10f
Juan Toledo <juansebastiantoledofreire@hotmail.com>
2021-12-31 14:56:16 -0300

    Adding embedMessage

--- index.js
+++ index.js
@@ -55,6 +55,27 @@ client.on("message", async message => {
             .addField("Campo en línea 3", "Puede tener un máximo de 25 campos.", true);
             message.channel.send({ embeds: [embedDatos] });
             break;
+
+        case "embedwelcome":
+            const user = message.mentions.users.first()
+            const embedDatos2 = new Discord.MessageEmbed() 
+            .setTitle("RULES:")
+            .setAuthor(client.user.username, client.user.displayAvatarURL())
+            .setColor(0x00AE86)
+            .setFooter("This is an importan message from the gods.", client.user.avatarURL())
+            .setTimestamp()
+            .setURL("https://github.com/CraterMaik")
+            .addField("Rule 1 - No spamming.", "This includes repeated use of bot commands, misuse of spoiler tags / code blocks / special text, rapidly switching voice channels or tagging people who are not currently active in the chat.")
+            .addField("Rule 2 - No advertising or selling.", "This server is not a marketplace: do not ask for money or in-game currency, try to buy / sell / giveaway anything, or ask people to join your Discord server or community. This includes messaging any user on the server.")
+            .addField("Rule 3 - Be respectful", "You must respect all users, regardless of your liking towards them. Treat others the way you want to be treated.")
+            .addField("Rule 4 - No hate speech or trolling.", "Harassment, hate speech, racism, sexism are not allowed here. This server has a zero-tolerance policy for such messages, and you may be banned immediately without warning or recourse.")
+            .addField(" REACT WITH ✅ TO GET VERIFIED AND HAVE ACCESS TO THE SERVER!!", "_");
+            message.channel.send({ embeds: [embedDatos2] }).then(embedDatos2 => {
+                embedDatos2.react("✅");
+            });
+            message.react.tick
+            break;
+
         case "help":
             message.channel.send("COMANDOS\n- hola \n- adios \n- mejor video \n- embedTest");
             break;

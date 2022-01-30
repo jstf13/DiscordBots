559994ca14a6da1d95b2a018bcb1a0951a3e657f
Juan Toledo <juansebastiantoledofreire@hotmail.com>
2022-01-30 10:39:53 -0300

    Saving last changes

--- Concil of Gods/counters/member_counter.js
+++ Concil of Gods/counters/member_counter.js
@@ -1,9 +1,14 @@
-module.exports = async (client) => {
-        const guild = client.guilds.cache.get("926465898582253618");
-        setInterval(() =>{
-            const memberCount = guild.memberCount;
-            const channel = guild.channel.cache.get('937133345932980224');
-            channel.setName(`Total Members: ${memberCount.toLocaleSring()}`);
-            console.log('Updating Member Count');
-        }, 5000);   
+var {client} = require(".././commands/commands_file")
+
+function countMembers() {
+    const guild = client.guilds.cache.get("926465898582253618");
+    console.log(guild.name);
+    setInterval(() =>{
+        const memberCount = guild.memberCount;
+        const channel = guild.channels.cache.get('937133345932980224');
+        channel.setName(`Total Members: ${memberCount}`);
+        console.log('Updating Member Count');
+    }, 5000);   
 }
+
+module.exports = {countMembers};

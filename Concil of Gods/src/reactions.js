bb6824df6a3532cb20f26540ae47867cb801934a
Juan Toledo <juansebastiantoledofreire@hotmail.com>
2022-03-09 17:39:25 -0300

    Adding tick reaction to welcome channel

--- Concil of Gods/src/reactions.js
+++ Concil of Gods/src/reactions.js
@@ -1,93 +1,121 @@
 const { GuildInviteManager } = require("discord.js");
-var {client} = require("../commands/commands_file")
+var { client } = require("../commands/commands_file");
 const config = require("../config.json");
 
-client.on('messageReactionAdd', async (reaction, user) => {
+client.on("messageReactionAdd", async (reaction, user) => {
+  let lenguageMessage = config.messagesIds.lenguage;
+  let welcomeMessage = config.messagesIds.welcome;
+  let welcomeMessageES = config.messagesIds.welcomeES;
 
-    let lenguageMessage = config.messagesIds.lenguage;
-    let welcomeMessage = config.messagesIds.welcome;
+  // When a reaction is received, check if the structure is partial
+  if (reaction.partial) {
+    // If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
+    try {
+      await reaction.fetch();
+    } catch (error) {
+      console.error("Something went wrong when fetching the message:", error);
+      // Return as `reaction.message.author` may be undefined/null
+      return;
+    }
+  }
 
-	// When a reaction is received, check if the structure is partial
-	if (reaction.partial) {
-		// If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
-		try {
-			await reaction.fetch();
-		} catch (error) {
-			console.error('Something went wrong when fetching the message:', error);
-			// Return as `reaction.message.author` may be undefined/null
-			return;
-		}
-	}
+  if (
+    (reaction.message.id == welcomeMessage ||
+      reaction.message.id == welcomeMessageES) &&
+    reaction.emoji.name === "✅"
+  ) {
+    try {
+      let commonRole = reaction.message.guild.roles.cache.get(
+        config.roles.sonOfGod
+      );
+      let reactedUser = reaction.message.guild.members.cache.find(
+        (member) => member.id === user.id
+      );
+      reactedUser.roles.add(commonRole);
+    } catch {
+      console.log(console.error, "Error : can't add the role");
+    }
+  }
 
-    if (reaction.message.id == welcomeMessage && reaction.emoji.name === '✅') {
-        try {
-            let commonRole = reaction.message.guild.roles.cache.get(config.roles.sonOfGod);
-            let reactedUser =  reaction.message.guild.members.cache.find(member => member.id === user.id)
-            reactedUser.roles.add(commonRole);
-         } catch {
-            console.log(console.error, 'Error : can\'t add the role');
-         }
+  if (reaction.message.id == lenguageMessage) {
+    let reactedUser = reaction.message.guild.members.cache.find(
+      (member) => member.id === user.id
+    );
+
+    if (reaction.emoji.name === "🇪🇸") {
+      let commonRole = reaction.message.guild.roles.cache.get(
+        config.roles.spanish
+      );
+      try {
+        reactedUser.roles.add(commonRole);
+      } catch {
+        console.log(console.error, "Error : can't add the spanish role");
+      }
     }
 
-    if (reaction.message.id == lenguageMessage) {
-        let reactedUser =  reaction.message.guild.members.cache.find(member => member.id === user.id)
-        
-        if (reaction.emoji.name === '🇪🇸') {
-            let commonRole = reaction.message.guild.roles.cache.get(config.roles.spanish);
-            try {
-                reactedUser.roles.add(commonRole);
-             } catch {
-                console.log(console.error, 'Error : can\'t add the spanish role');
-             }
-        }
-        
-        if (reaction.emoji.name === '🇬🇧') {
-            let commonRole = reaction.message.guild.roles.cache.get(config.roles.english);    
-            try {
-                reactedUser.roles.add(commonRole);
-             } catch {
-                console.log(console.error, 'Error : can\'t add the english role');
-             }
-        }
+    if (reaction.emoji.name === "🇬🇧") {
+      let commonRole = reaction.message.guild.roles.cache.get(
+        config.roles.english
+      );
+      try {
+        reactedUser.roles.add(commonRole);
+      } catch {
+        console.log(console.error, "Error : can't add the english role");
+      }
     }
+  }
 });
 
-client.on('messageReactionRemove', (reaction, user) => {
-    
-    let lenguageMessage = 927573907760881665;
-    let welcomeMessage = 926960221031636993;
+client.on("messageReactionRemove", (reaction, user) => {
+  let lenguageMessage = config.messagesIds.lenguage;
+  let welcomeMessage = config.messagesIds.welcome;
+  let welcomeMessageES = config.messagesIds.welcomeES;
 
-    if (reaction.message.id == 926960221031636993 && reaction.emoji.name === '✅') {
-        let commonRole = reaction.message.guild.roles.cache.get(config.roles.sonOfGod);
-        let reactedUser =  reaction.message.guild.members.cache.find(member => member.id === user.id)
-        try {
-            reactedUser.roles.remove(commonRole);
-         } catch {
-            console.log(console.error, 'Error : can\'t remove the role');
-         }
+  if (
+    (reaction.message.id == welcomeMessage ||
+      reaction.message.id == welcomeMessageES) &&
+    reaction.emoji.name === "✅"
+  ) {
+    let commonRole = reaction.message.guild.roles.cache.get(
+      config.roles.sonOfGod
+    );
+    let reactedUser = reaction.message.guild.members.cache.find(
+      (member) => member.id === user.id
+    );
+    try {
+      reactedUser.roles.remove(commonRole);
+    } catch {
+      console.log(console.error, "Error : can't remove the role");
+    }
+  }
+
+  if (reaction.message.id == lenguageMessage) {
+    let reactedUser = reaction.message.guild.members.cache.find(
+      (member) => member.id === user.id
+    );
+
+    if (reaction.emoji.name === "🇪🇸") {
+      let commonRole = reaction.message.guild.roles.cache.get(
+        config.roles.spanish
+      );
+      try {
+        reactedUser.roles.remove(commonRole);
+      } catch {
+        console.log(console.error, "Error : can't remove the spanish role");
+      }
     }
 
-    if (reaction.message.id == lenguageMessage) {
-        let reactedUser =  reaction.message.guild.members.cache.find(member => member.id === user.id)
-        
-        if (reaction.emoji.name === '🇪🇸') {
-            let commonRole = reaction.message.guild.roles.cache.get(config.roles.spanish);
-            try {
-                reactedUser.roles.remove(commonRole);
-             } catch {
-                console.log(console.error, 'Error : can\'t remove the spanish role');
-             }
-        }
-        
-        if (reaction.emoji.name === '🇬🇧') {
-            let commonRole = reaction.message.guild.roles.cache.get(config.roles.english);   
-            try {
-                reactedUser.roles.remove(commonRole);
-             } catch {
-                console.log(console.error, 'Error : can\'t remove the english role');
-             }
-        }
+    if (reaction.emoji.name === "🇬🇧") {
+      let commonRole = reaction.message.guild.roles.cache.get(
+        config.roles.english
+      );
+      try {
+        reactedUser.roles.remove(commonRole);
+      } catch {
+        console.log(console.error, "Error : can't remove the english role");
+      }
     }
+  }
 });
 
 /*--- Close Reaction zone ---*/
